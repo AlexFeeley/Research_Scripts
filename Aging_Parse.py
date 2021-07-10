c46ac2753f0842715cc0e37d724a1ab12b4587e9
yonixiong <yoni.xiong@vanderbilt.edu>
2021-07-10 00:59:08 -0500

    cleanup

--- Aging_Parse.py
+++ Aging_Parse.py
@@ -1,104 +1,103 @@
 from queue import Queue
 
 # Number of ring oscillators
-RO_NUM = 16
-
+RO_NUM = 14
 fRO = "C20_1048mV_Copy.csv"
+
+def update_averages(frequency, frequencyIndex):
+    print(frequencyIndex)
+    print(averages)
+    print(counts)
+    if frequencyIndex > len(averages):
+        averages.append(frequency)
+        counts.append(1)
+    else:        
+        averages[frequencyIndex] = (averages[frequencyIndex] * counts[frequencyIndex] + frequency) / (counts[frequencyIndex] + 1)
+        counts[frequencyIndex] += 1
+    print("done")
+
 f = open(fRO, "rt")
-filename = fRO + "_post.txt"
+filename = fRO + "_post.csv"
 output = open(filename, "w+")
 
-# Create an empty q's to keep data
-q1 = Queue(maxsize=0)
-q2 = Queue(maxsize=0)
-q3 = Queue(maxsize=0)
-q4 = Queue(maxsize=0)
-q5 = Queue(maxsize=0)
-q6 = Queue(maxsize=0)
-q7 = Queue(maxsize=0)
-q8 = Queue(maxsize=0)
-q9 = Queue(maxsize=0)
-q10 = Queue(maxsize=0)
-q11 = Queue(maxsize=0)
-q12 = Queue(maxsize=0)
-q13 = Queue(maxsize=0)
-q14 = Queue(maxsize=0)
-q15 = Queue(maxsize=0)
-q16 = Queue(maxsize=0)
+
+queueList = []
+for i in range(RO_NUM):
+    queueList.append(Queue(maxsize=0))
 
 # Init error line, counter, and errorSR values 
-freqCounter = 1
+freqCounter = 0
 counter = 0
-i = 0
 minDiff = 100
 
 # treat first line different
 first = f.readline()
+first = f.readline()
+first = f.readline()
+first = f.readline()
+first = f.readline()
+first = f.readline()
+first = f.readline()
+first = f.readline()
+first = f.readline()
+
+
+# read the real data
 temp = first.split(",")
-tempFreq = float(temp[1])
+prevFreq = float(temp[1])
+
+# freq set
+freqSet = [prevFreq]
+setCounter = 1
 
 # Parse through all data in file
 for line in f:
     
-    if counter < 100:
+    if counter < 50: 
         # Split line at ,
         temp = line.split(",")
 
         time = temp[0]
         freq = float(temp[1])
 
-        diff = abs(freq - tempFreq)
+        diff = abs(freq - prevFreq)
+        print(freq)
 
-        if diff > 1000000: 
-            # different frequency!
-            if freqCounter == 1: 
-                q1.put(tempFreq)
-            if freqCounter == 2: 
-                q2.put(tempFreq)
-            if freqCounter == 3: 
-                q3.put(tempFreq)
-            if freqCounter == 4: 
-                q4.put(tempFreq)
-            if freqCounter == 5: 
-                q5.put(tempFreq)
-            if freqCounter == 6: 
-                q6.put(tempFreq)
-            if freqCounter == 7: 
-                q7.put(tempFreq)
-            if freqCounter == 8: 
-                q8.put(tempFreq)
-            if freqCounter == 9: 
-                q9.put(tempFreq)
-            if freqCounter == 10: 
-                q10.put(tempFreq)
-            if freqCounter == 11: 
-                q11.put(tempFreq)
-            if freqCounter == 12: 
-                q12.put(tempFreq)
-            if freqCounter == 13: 
-                q13.put(tempFreq)
-            if freqCounter == 14: 
-                q14.put(tempFreq)
-            if freqCounter == 15: 
-                q15.put(tempFreq)
-            if freqCounter == 16: 
-                q16.put(tempFreq)
+        # just skip the first two ROs
+        if freq > 80000000: 
+            continue
+
+        if diff > 200000 : 
+            # different frequency
+            # take average of freqSet, clear it, and reset counter to zero 
+            average = sum(freqSet)/setCounter
+
+            # start new list and increment counter 
+            freqSet = [freq,0,0,0,0,0,0]
+            setCounter = 1
+            queueList[freqCounter].put(average)
             freqCounter += 1
-            if freqCounter > RO_NUM :
-                freqCounter = 1
-            print(freqCounter)
-        tempFreq = freq
-        counter += 1
-temp1 = q1.get()
-print(temp1)
-temp1 = q1.get()
-print(temp1)
+            if freqCounter > RO_NUM - 1 :
+                freqCounter = 0
+            if freqCounter == 8: 
+                if freq > 11E+6: 
+                    # skip
+                    freqCounter += 1
+        else: 
+            # part of same freq set, add to array
+            freqSet[setCounter] = freq
+            setCounter += 1
+            
+        prevFreq = freq
+        counter +=1
 
-
-temp16 = q16.get()
-print(temp16)
-temp16 = q16.get()
-print(temp16)
+tempString = ''
+for freqQueue in queueList:
+    # print(list(freqQueue.queue))
+    # csvString = ''.join(list(freqQueue.queue))
+    csvString = ','.join([str(i) for i in list(freqQueue.queue)])
+    output.write(csvString)
+    output.write("\n")
 
 # print("%2d, %2d" % (temp1,temp2))
 # print("\n")
